1e87a3beb5c440efa61ae9b6ea6adf9feefcf01b
Reid Netterville III <reidiiius@gmail.com>
2014-09-10 15:18:31 -0500

    140910.0

---
+++ scripts/vgqnxhr.js
@@ -0,0 +1,92 @@
+/* Copyright 2014 Reid Netterville III */
+
+function gQn(resId) {
+  var xhrq = new XMLHttpRequest()
+  xhrq.open("get", resId, false); xhrq.send()
+  document.getElementById("gqn").innerHTML=xhrq.responseText
+}
+
+// obv
+function gqn0() { gQn("/guqin/vrt/obv/gqn0.txt") }
+function gqk6() { gQn("/guqin/vrt/obv/gqk6.txt") }
+function gqj17() { gQn("/guqin/vrt/obv/gqj17.txt") }
+function gqk6x5() { gQn("/guqin/vrt/obv/gqk6x5.txt") }
+function gqj17y2() { gQn("/guqin/vrt/obv/gqj17y2.txt") }
+function gqj3() { gQn("/guqin/vrt/obv/gqj3.txt") }
+function gqj17k2() { gQn("/guqin/vrt/obv/gqj17k2.txt") }
+function gqn26y5() { gQn("/guqin/vrt/obv/gqn26y5.txt") }
+function gqk26x5() { gQn("/guqin/vrt/obv/gqk26x5.txt") }
+function gqj6() { gQn("/guqin/vrt/obv/gqj6.txt") }
+function gqj36() { gQn("/guqin/vrt/obv/gqj36.txt") }
+function gqk56() { gQn("/guqin/vrt/obv/gqk56.txt") }
+function gqj136y7() { gQn("/guqin/vrt/obv/gqj136y7.txt") }
+function gqk56x4() { gQn("/guqin/vrt/obv/gqk56x4.txt") }
+function gqn167x4() { gQn("/guqin/vrt/obv/gqn167x4.txt") }
+function gqj3k5x4() { gQn("/guqin/vrt/obv/gqj3k5x4.txt") }
+function gqj167y2() { gQn("/guqin/vrt/obv/gqj167y2.txt") }
+function gqj2() { gQn("/guqin/vrt/obv/gqj2.txt") }
+function gqj236() { gQn("/guqin/vrt/obv/gqj236.txt") }
+function gqj26() { gQn("/guqin/vrt/obv/gqj26.txt") }
+function gqj23() { gQn("/guqin/vrt/obv/gqj23.txt") }
+function gqj23k6() { gQn("/guqin/vrt/obv/gqj23k6.txt") }
+function gqj2y3() { gQn("/guqin/vrt/obv/gqj2y3.txt") }
+function gqj2k6() { gQn("/guqin/vrt/obv/gqj2k6.txt") }
+function gqj26y3() { gQn("/guqin/vrt/obv/gqj26y3.txt") }
+function gqj2k56() { gQn("/guqin/vrt/obv/gqj2k56.txt") }
+function gqj246y3() { gQn("/guqin/vrt/obv/gqj246y3.txt") }
+function gqj2k56x4() { gQn("/guqin/vrt/obv/gqj2k56x4.txt") }
+function gqk157x6() { gQn("/guqin/vrt/obv/gqk157x6.txt") }
+function gqj26y34() { gQn("/guqin/vrt/obv/gqj26y34.txt") }
+function gqj2k6x5() { gQn("/guqin/vrt/obv/gqj2k6x5.txt") }
+function gqj2k6y3() { gQn("/guqin/vrt/obv/gqj2k6y3.txt") }
+function gqk1j6() { gQn("/guqin/vrt/obv/gqk1j6.txt") }
+function gqn345() { gQn("/guqin/vrt/obv/gqn345.txt") }
+function gqj3k6() { gQn("/guqin/vrt/obv/gqj3k6.txt") }
+function gqn45y2() { gQn("/guqin/vrt/obv/gqn45y2.txt") }
+function gqk2j6() { gQn("/guqin/vrt/obv/gqk2j6.txt") }
+function gqn5y2() { gQn("/guqin/vrt/obv/gqn5y2.txt") }
+function gqk26() { gQn("/guqin/vrt/obv/gqk26.txt") }
+function gqk256() { gQn("/guqin/vrt/obv/gqk256.txt") }
+
+// rev
+function gqz0() { gQn("/guqin/vrt/rev/gqz0.txt") }
+function gqj5() { gQn("/guqin/vrt/rev/gqj5.txt") }
+function gqk34() { gQn("/guqin/vrt/rev/gqk34.txt") }
+function gqj5y6() { gQn("/guqin/vrt/rev/gqj5y6.txt") }
+function gqk34x2() { gQn("/guqin/vrt/rev/gqk34x2.txt") }
+function gqk1() { gQn("/guqin/vrt/rev/gqk1.txt") }
+function gqj2k34() { gQn("/guqin/vrt/rev/gqj2k34.txt") }
+function gqn25x6() { gQn("/guqin/vrt/rev/gqn25x6.txt") }
+function gqj25y6() { gQn("/guqin/vrt/rev/gqj25y6.txt") }
+function gqk5() { gQn("/guqin/vrt/rev/gqk5.txt") }
+function gqk15() { gQn("/guqin/vrt/rev/gqk15.txt") }
+function gqj56() { gQn("/guqin/vrt/rev/gqj56.txt") }
+function gqk135x4() { gQn("/guqin/vrt/rev/gqk135x4.txt") }
+function gqj56y7() { gQn("/guqin/vrt/rev/gqj56y7.txt") }
+function gqn345y7() { gQn("/guqin/vrt/rev/gqn345y7.txt") }
+function gqk1j6y7() { gQn("/guqin/vrt/rev/gqk1j6y7.txt") }
+function gqk345x2() { gQn("/guqin/vrt/rev/gqk345x2.txt") }
+function gqk2() { gQn("/guqin/vrt/rev/gqk2.txt") }
+function gqk125() { gQn("/guqin/vrt/rev/gqk125.txt") }
+function gqk25() { gQn("/guqin/vrt/rev/gqk25.txt") }
+function gqk12() { gQn("/guqin/vrt/rev/gqk12.txt") }
+function gqk12j5() { gQn("/guqin/vrt/rev/gqk12j5.txt") }
+function gqk2x1() { gQn("/guqin/vrt/rev/gqk2x1.txt") }
+function gqk2j5() { gQn("/guqin/vrt/rev/gqk2j5.txt") }
+function gqk25x1() { gQn("/guqin/vrt/rev/gqk25x1.txt") }
+function gqk2j56() { gQn("/guqin/vrt/rev/gqk2j56.txt") }
+function gqk257x1() { gQn("/guqin/vrt/rev/gqk257x1.txt") }
+function gqk2j56y7() { gQn("/guqin/vrt/rev/gqk2j56y7.txt") }
+function gqj346y5() { gQn("/guqin/vrt/rev/gqj346y5.txt") }
+function gqk25x17() { gQn("/guqin/vrt/rev/gqk25x17.txt") }
+function gqk2j5y6() { gQn("/guqin/vrt/rev/gqk2j5y6.txt") }
+function gqk2j5x1() { gQn("/guqin/vrt/rev/gqk2j5x1.txt") }
+function gqj3k5() { gQn("/guqin/vrt/rev/gqj3k5.txt") }
+function gqn167() { gQn("/guqin/vrt/rev/gqn167.txt") }
+function gqk1j5() { gQn("/guqin/vrt/rev/gqk1j5.txt") }
+function gqn67x2() { gQn("/guqin/vrt/rev/gqn67x2.txt") }
+function gqj2k5() { gQn("/guqin/vrt/rev/gqj2k5.txt") }
+function gqn6x2() { gQn("/guqin/vrt/rev/gqn6x2.txt") }
+function gqj25() { gQn("/guqin/vrt/rev/gqj25.txt") }
+function gqj256() { gQn("/guqin/vrt/rev/gqj256.txt") }
+
