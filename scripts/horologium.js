06d899f59d606968e5d7251a98c1b6592fab155e
Reid Netterville III <reidiiius@gmail.com>
2015-09-06 20:58:58 -0500

    150906.1

---
+++ scripts/horologium.js
@@ -0,0 +1,13 @@
+// Copyright 2015 Reid Netterville III
+
+"use strict";
+
+function chronoMetric(o) {
+
+var gebi = document.getElementById(o);
+var nwdt = new Date();
+
+  gebi.textContent = nwdt.getTime();
+
+}
+
--- scripts/stellar.js
+++ scripts/stellar.js
@@ -8,17 +8,17 @@ function celestial(resId) {
   document.getElementById("view").innerHTML = xhrq.responseText;
 }
 
-function castorPollux()  { celestial("/beadgcf/reliquary/agate.txt")      }
-function alpherg()       { celestial("/beadgcf/reliquary/amethyst.txt")   }
-function antares()       { celestial("/beadgcf/reliquary/beryl.txt")      }
-function hamal()         { celestial("/beadgcf/reliquary/bloodstone.txt") }
-function spica()         { celestial("/beadgcf/reliquary/carnelian.txt")  }
-function acubens()       { celestial("/beadgcf/reliquary/emerald.txt")    }
-function sadalmelik()    { celestial("/beadgcf/reliquary/garnet.txt")     }
-function regulus()       { celestial("/beadgcf/reliquary/onyx.txt")       }
-function zubenelgenubi() { celestial("/beadgcf/reliquary/peridot.txt")    }
-function algedi()        { celestial("/beadgcf/reliquary/ruby.txt")       }
-function aldebaran()     { celestial("/beadgcf/reliquary/sapphire.txt")   }
-function rukbat()        { celestial("/beadgcf/reliquary/topaz.txt")      }
+function castorPollux() { celestial("\/beadgcf\/reliquary\/agate.txt")      }
+function alpherg()      { celestial("\/beadgcf\/reliquary\/amethyst.txt")   }
+function antares()      { celestial("\/beadgcf\/reliquary\/beryl.txt")      }
+function hamal()        { celestial("\/beadgcf\/reliquary\/bloodstone.txt") }
+function spica()        { celestial("\/beadgcf\/reliquary\/carnelian.txt")  }
+function acubens()      { celestial("\/beadgcf\/reliquary\/emerald.txt")    }
+function sadalmelik()   { celestial("\/beadgcf\/reliquary\/garnet.txt")     }
+function regulus()      { celestial("\/beadgcf\/reliquary\/onyx.txt")       }
+function zubenelgenubi(){ celestial("\/beadgcf\/reliquary\/peridot.txt")    }
+function algedi()       { celestial("\/beadgcf\/reliquary\/ruby.txt")       }
+function aldebaran()    { celestial("\/beadgcf\/reliquary\/sapphire.txt")   }
+function rukbat()       { celestial("\/beadgcf\/reliquary\/topaz.txt")      }
 
 
