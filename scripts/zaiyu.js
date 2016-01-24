8f792df7f1cdc106dcea060f65d35fd8b7546b27
Reid Netterville III <reidiiius@gmail.com>
2016-01-23 22:26:23 -0600

    160123.0

--- scripts/zaiyu.js
+++ scripts/zaiyu.js
@@ -1,11 +1,11 @@
-// Copyright 2015 Reid Netterville III
+// Copyright 2015-2016 Reid Netterville III
 
 "use strict";
 
 (function() {
   var f = [];
   for (var n = 0; n < 49; n++) {
-    f.push(Math.pow(2, n / 12) * 1);
+    f.push(Math.pow(2, n / 12).toFixed(15) * 1);
   }
   var z = document.getElementById("zhu");
   z.textContent = f.join("\n");
