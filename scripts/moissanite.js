c59df2e992d315f512e842edf8cc2dace9e3a83c
Reid Netterville III <reidiiius@gmail.com>
2017-05-15 01:18:02 -0500

    170515.0

---
+++ scripts/moissanite.js
@@ -0,0 +1,205 @@
+// Copyright (c) 2017 Reid Netterville III
+
+"use strict";
+
+var Panopolis = {};
+
+Panopolis.amalgam = {
+  j136y7: [  0,  0,122,181,  0,167,  0,137,196, 98,  0, 76],
+  j167y2: [152,  0,  0,  0, 91,167,  0,137,196,  0, 38, 76],
+     j17: [  0,  0,120,  0, 90,165,  0,135,  0,105, 36, 66],
+   j17k2: [  0,  0,  0, 52, 82,166,  0,136,  0,106, 37, 67],
+   j17y2: [150,  0,  0,  0, 90,165,  0,135,  0,105, 36, 66],
+      j2: [153,228,  0,  0, 92,168,  0,138,  0,107,  0, 78],
+     j23: [153,228,  0,182,  0,168,  0,138,  0,107,  0, 78],
+    j236: [153,228,  0,182,  0,168,  0,138,197,  0,  0, 78],
+   j23k6: [153,228,  0,182,  0,168,  0,138,  0,  0, 39, 78],
+  j246y3: [153,228,114,  0, 92,  0,  0,138,197,  0,  0, 78],
+    j256: [ 69,147,  0,  0, 57, 84,162,  0,140,  0,  0, 42],
+     j25: [ 69,147,  0,  0, 57, 84,162,  0,  0,200,  0, 42],
+   j25y6: [ 37, 67,  0,  0,121, 52, 82,166,  0,  0,  0,106],
+     j26: [153,228,  0,  0, 92,168,  0,138,197,  0,  0, 78],
+   j26y3: [153,228,114,  0,  0,168,  0,138,197,  0,  0, 78],
+  j26y34: [153,228,114,182,  0,  0,  0,138,197,  0,  0, 78],
+   j2k34: [ 37, 67,  0,  0,  0, 52, 82,166,  0,136,  0,106],
+   j2k56: [153,228,  0,  0, 92,168,  0,  0,197,  0, 39, 78],
+    j2k5: [ 69,147,  0,  0, 57, 84,  0,  0,140,200,  0, 42],
+ j2k56x4: [153,228,  0,  0, 92,  0,  0,138,197,  0, 39, 78],
+    j2k6: [153,228,  0,  0, 92,168,  0,138,  0,  0, 39, 78],
+  j2k6x5: [153,228,  0,  0, 92,168,  0,  0,  0,107, 39, 78],
+  j2k6y3: [153,228,114,  0,  0,168,  0,138,  0,  0, 39, 78],
+    j2y3: [153,228,114,  0,  0,168,  0,138,  0,107,  0, 78],
+      j3: [151,  0,121,180,  0,166,  0,136,  0,106,  0, 75],
+  j346y5: [197,  0, 39, 78,153,228,  0,  0, 92,  0,  0,138],
+   j34k6: [151,  0,121, 52, 82,  0,  0,136,  0,  0, 37, 67],
+     j36: [152,  0,122,181,  0,167,  0,137,196,  0,  0, 76],
+    j3k5: [197,  0, 39, 78,  0,228,  0,  0, 92,168,  0,138],
+ j3k56x4: [146,  0,124,184,  0,  0,  0,139,199,  0, 41, 68],
+  j3k5x4: [152,  0,122,181,  0,  0,  0,137,196, 98,  0, 76],
+    j3k6: [146,  0,124,184,  0,170,  0,139,  0,  0, 41, 68],
+      j5: [165,  0,135,  0,105, 36, 66,  0,  0,120,  0, 90],
+     j56: [181,  0,167,  0,137,196, 98,  0, 76,  0,  0,122],
+   j56y7: [181,  0,167,  0,137,196, 98,  0, 76,152,  0,  0],
+    j5y6: [165,  0,135,  0,105, 36, 66,150,  0,  0,  0, 90],
+      j6: [152,  0,122,  0, 91,167,  0,137,196,  0,  0, 76],
+      k1: [  0, 75,151,  0,121,180,  0,166,  0,136,  0,106],
+     k12: [  0,107,  0, 78,153,228,  0,182,  0,168,  0,138],
+    k125: [  0,107,  0, 78,153,228,  0,  0, 92,168,  0,138],
+   k12j5: [  0,107,  0, 78,153,228,114,  0,  0,168,  0,138],
+  k135x4: [  0, 91,167,  0,  0,196,  0, 38, 76,152,  0,122],
+     k15: [  0, 91,167,  0,137,196,  0,  0, 76,152,  0,122],
+  k157x6: [153,228,114,  0, 92,168,  0,  0,197,  0,  0, 78],
+   k17j5: [ 37, 67,151,  0,121, 52, 82,  0,  0,136,  0,  0],
+    k1j5: [  0,139,199,  0, 41, 68,146,  0,  0,184,  0,170],
+ k1j56y7: [  0,139,199,  0, 41, 68,146,  0,124,184,  0,  0],
+    k1j6: [  0,228,114,  0, 92,168,  0,138,197,  0,  0, 78],
+  k1j6y7: [  0, 91,167,  0,137,196,  0, 38, 76,152,  0,  0],
+      k2: [197,  0,  0, 78,153,228,  0,182,  0,168,  0,138],
+     k25: [197,  0,  0, 78,153,228,  0,  0, 92,168,  0,138],
+    k256: [147,  0,  0, 57, 84,162,  0,  0,200,  0, 42, 69],
+  k257x1: [197,  0, 39, 78,153,228,  0,  0, 92,168,  0,  0],
+   k25x1: [  0,  0, 39, 78,153,228,  0,  0, 92,168,  0,138],
+  k25x17: [  0,107, 39, 78,153,228,  0,  0, 92,168,  0,  0],
+     k26: [147,  0,  0, 57, 84,162,  0,140,  0,  0, 42, 69],
+   k26x5: [151,  0,  0, 52, 82,166,  0,  0,  0,106, 37, 67],
+    k2j5: [197,  0,  0, 78,153,228,114,  0,  0,168,  0,138],
+   k2j56: [197,  0,  0, 78,153,228,114,  0, 92,  0,  0,138],
+ k2j56y7: [197,  0,  0, 78,153,228,114,  0, 92,168,  0,  0],
+  k2j5x1: [  0,  0, 39, 78,153,228,114,  0,  0,168,  0,138],
+  k2j5y6: [197,  0,  0, 78,153,228,114,182,  0,  0,  0,138],
+    k2j6: [147,  0,  0, 57, 84,162,  0,140,200,  0,  0, 69],
+    k2x1: [  0,  0, 39, 78,153,228,  0,182,  0,168,  0,138],
+     k34: [165,  0,135,  0,  0, 36, 66,150,  0,120,  0, 90],
+  k345x2: [181,  0,  0,  0,137,196, 98,  0, 76,152,  0,122],
+   k34x2: [165,  0,  0,  0,105, 36, 66,150,  0,120,  0, 90],
+      k5: [181,  0,167,  0,137,196,  0,  0, 76,152,  0,122],
+     k56: [152,  0,122,  0, 91,167,  0,  0,196,  0, 38, 76],
+   k56x4: [152,  0,122,  0, 91,  0,  0,137,196,  0, 38, 76],
+      k6: [150,  0,120,  0, 90,165,  0,135,  0,  0, 36, 66],
+    k6x5: [150,  0,120,  0, 90,165,  0,  0,  0,105, 36, 66],
+      n0: [149,  0,119,  0, 89,164,  0,134,  0,104,  0, 74],
+    n167: [197,  0,  0, 78,  0,228,114,  0, 92,168,  0,138],
+  n167x4: [152,  0,  0,181,  0,167,  0,137,196, 98,  0, 76],
+   n25x6: [ 37, 67,151,  0,  0, 52, 82,166,  0,  0,  0,106],
+   n26y5: [  0,  0,121, 52, 82,166,  0,  0,  0,106, 37, 67],
+    n345: [  0,228,  0,  0, 92,168,  0,138,197,  0, 39, 78],
+  n345y7: [  0, 91,  0,  0,137,196,  0, 38, 76,152,  0,122],
+   n45y2: [146,  0,  0,184,  0,170,  0,139,199,  0, 41, 68],
+    n5y2: [147,  0,  0, 57, 84,  0,  0,140,200,  0, 42, 69],
+   n67x2: [  0,139,  0,  0, 41, 68,146,  0,124,184,  0,170],
+    n6x2: [ 69,147,  0,  0, 57, 84,162,  0,140,200,  0,  0]
+};
+
+Panopolis.orthodigraph = {
+  gj: function(qp) {return(qp.slice(18,36).concat(qp.slice(0,18)))},
+  dj: function(qp) {return(qp.slice( 3,36).concat(qp.slice(0, 3)))},
+  aj: function(qp) {return(qp.slice(24,36).concat(qp.slice(0,24)))},
+  ej: function(qp) {return(qp.slice( 9,36).concat(qp.slice(0, 9)))},
+  bj: function(qp) {return(qp.slice(30,36).concat(qp.slice(0,30)))},
+  fn: function(qp) {return(qp.slice(15,36).concat(qp.slice(0,15)))},
+  cn: function(qp) {return(qp.slice( 0,36).concat(qp.slice(0, 0)))},
+  gn: function(qp) {return(qp.slice(21,36).concat(qp.slice(0,21)))},
+  dn: function(qp) {return(qp.slice( 6,36).concat(qp.slice(0, 6)))},
+  an: function(qp) {return(qp.slice(27,36).concat(qp.slice(0,27)))},
+  en: function(qp) {return(qp.slice(12,36).concat(qp.slice(0,12)))},
+  bn: function(qp) {return(qp.slice(33,36).concat(qp.slice(0,33)))},
+  fk: function(qp) {return(qp.slice(18,36).concat(qp.slice(0,18)))},
+  ck: function(qp) {return(qp.slice( 3,36).concat(qp.slice(0, 3)))},
+  gk: function(qp) {return(qp.slice(24,36).concat(qp.slice(0,24)))},
+  dk: function(qp) {return(qp.slice( 9,36).concat(qp.slice(0, 9)))},
+  ak: function(qp) {return(qp.slice(30,36).concat(qp.slice(0,30)))}
+};
+
+Panopolis.nystrom = function(fortecken) {
+  let datarr = this.amalgam[fortecken];
+  let tmparr = [];
+    for (var i = 0; i < datarr.length; i++) {
+      tmparr.push(datarr[i].toString(16));
+    }
+  let novast = tmparr.join(" ").toUpperCase() + " ";
+  let result = novast.replace(/0/g, "__");
+  return result;
+};
+
+Panopolis.gamut = function(ndx, qp) {
+  let sym = "orthodigraph";
+  let arr = [
+    this[sym].fn(qp),
+    this[sym].cn(qp),
+    this[sym].gn(qp),
+    this[sym].dn(qp),
+    this[sym].an(qp),
+    this[sym].en(qp),
+    this[sym].bn(qp),
+    this[sym].fk(qp),
+    this[sym].ck(qp),
+    this[sym].gk(qp),
+    this[sym].dk(qp),
+    this[sym].ak(qp)
+  ];
+  return arr[ndx];
+};
+
+function inStrum() {
+  let qp = arguments[0], tng = arguments[1], arr = arguments[2].reverse();
+  document.writeln("\n  " + qp + '-' + tng + '-hx' + Panopolis.serialStamp);
+  for (let i = 0; i < arr.length; i++) {
+    document.writeln("  " + Panopolis.gamut(arr[i], Panopolis.nystrom(qp)));
+  }
+}
+
+Panopolis.stringTuner = function(qp) {
+  var arr = [];
+  for (let i = 11; i >= 0; i--) arr.push(i);
+  inStrum(qp, 'CXLIV', arr);
+};
+
+Panopolis.beadgcf = function(qp) {
+  inStrum(qp, 'beadgcf', [6, 5, 4, 3, 2, 1, 0]);
+};
+
+Panopolis.bfbfb = function(qp) {
+  inStrum(qp, 'bfbfb', [6, 0, 6, 0, 6]);
+};
+
+Panopolis.cgdae = function(qp) {
+  inStrum(qp, 'cgdae', [1, 2, 3, 4, 5]);
+};
+
+Panopolis.dadgad = function(qp) {
+  inStrum(qp, 'dadgad', [3, 4, 3, 2, 4, 3]);
+};
+
+Panopolis.dgdgbd = function(qp) {
+  inStrum(qp, 'dgdgbd', [3, 2, 3, 2, 6, 3]);
+};
+
+Panopolis.eadgbe = function(qp) {
+  inStrum(qp, 'eadgbe', [5, 4, 3, 2, 6, 5]);
+};
+
+Panopolis.fkbjdn = function(qp) {
+  inStrum(qp, 'fkbjdn', [7, 11, 3, 7, 11, 3]);
+};
+
+Panopolis.chronoMetric = function() {
+  let nwdt = new Date();
+  return nwdt.getTime();
+};
+
+Panopolis.serialStamp = Panopolis.chronoMetric();
+
+Panopolis.harlequin = function() {
+  for (let i in Panopolis.amalgam) {
+    Panopolis.stringTuner(i);
+    document.writeln();
+  }
+}
+
+Panopolis.tuning = function(str) {
+  for (let i in Panopolis.amalgam) {
+    Panopolis[str](i);
+    document.writeln();
+  }
+}
+
